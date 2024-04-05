8361c35e4a3a2cce616f23706746ac9c09f1eab6
Benjaminatedu <dtytrewq@hotmail.com>
2024-04-04 21:40:03 -0600

    changed something

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -17,7 +17,7 @@ class TestSmokeTest():
         cls.driver.quit()
 
     def test_admin_page_username_password(self):
-        self.driver.get("http://127.0.0.1:8000/teton/1.6/index.html")
+        self.driver.get("https://benjaminatedu.github.io/cse270-teton/index.html")
         self.driver.set_window_size(1294, 1392)
         self.driver.find_element(By.LINK_TEXT, "Admin").click()
         elements = self.driver.find_elements(By.ID, "username")
@@ -29,7 +29,7 @@ class TestSmokeTest():
         assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
 
     def test_home_page_spotlights(self):
-        self.driver.get("http://127.0.0.1:8000/teton/1.6/index.html")
+        self.driver.get("https://benjaminatedu.github.io/cse270-teton/index.html")
         self.driver.set_window_size(1294, 1392)
         elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1 > .centered-image")
         assert len(elements) > 0
@@ -41,7 +41,7 @@ class TestSmokeTest():
         assert self.driver.find_element(By.CSS_SELECTOR, "section > h3").text == "Welcome to the Teton Chamber of Commerce Signup Wizard!"
 
     def test_join_page_data_entry(self):
-        self.driver.get("http://127.0.0.1:8000/teton/1.6/index.html")
+        self.driver.get("https://benjaminatedu.github.io/cse270-teton/index.html")
         self.driver.set_window_size(1294, 1392)
         self.driver.find_element(By.LINK_TEXT, "Join").click()
         elements = self.driver.find_elements(By.NAME, "fname")
@@ -58,7 +58,7 @@ class TestSmokeTest():
         assert len(elements) > 0
 
     def test_directory_grid_and_list(self):
-        self.driver.get("http://127.0.0.1:8000/teton/1.6/index.html")
+        self.driver.get("https://benjaminatedu.github.io/cse270-teton/index.html")
         self.driver.set_window_size(1294, 1392)
         self.driver.find_element(By.LINK_TEXT, "Directory").click()
         elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9)")
@@ -68,7 +68,7 @@ class TestSmokeTest():
         assert len(elements) > 0
 
     def test_logo_header_and_title(self):
-        self.driver.get("http://127.0.0.1:8000/teton/1.6/index.html")
+        self.driver.get("https://benjaminatedu.github.io/cse270-teton/index.html")
         self.driver.set_window_size(1294, 1392)
         elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
         assert len(elements) > 0
